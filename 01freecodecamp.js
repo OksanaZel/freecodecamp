14a29a15907c873e3308d8978cb25c8c466a321b
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-02-19 23:03:23 +0200

    Добавляет задачу

---
+++ 01freecodecamp.js
@@ -0,0 +1,22 @@
+// function nextInLine(arr, item) {
+//     arr.push(item);
+//     item = arr.shift(arr[0]);
+//     // console.log(arr);
+//   // Only change code below this line
+  
+//   return arr;
+//   // Only change code above this line
+// }
+
+// console.log(nextInLine([], 5));//5
+// console.log(nextInLine([], 1));//1
+// console.log(nextInLine([2], 1));//2
+// console.log(nextInLine([5,6,7,8,9], 1));//5
+// console.log(nextInLine([4], 10));
+
+// var testArr = [4];
+
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log("After: " + JSON.stringify(testArr));
+
